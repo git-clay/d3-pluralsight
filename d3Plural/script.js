27c8b88cbdf685635932bf240da36142c7c8df31
git-clay <ce.smith5525@gmail.com>
2018-06-24 11:43:16 -0600

    forceddirected example

--- d3Plural/script.js
+++ d3Plural/script.js
@@ -230,3 +230,107 @@ d3.select('select')
             .text(d => showMinMax(lineData, 'sales', d.sales, sel))
     })
 //#endregion
+//#region force-directed relation graph -- broken
+const relationData = (d3.json("https://cdn.rawgit.com/mbostock/4062045" +
+    "/raw/5916d145c8c048a6e3086915a6be464467391c62" +
+    "/miserables.json"))
+let links, link, nodes, node;
+relationData.then(res => {
+    links = res.links.map(d => Object.create(d));
+    createLine();
+});
+relationData.then(res => {
+    nodes = res.nodes.map(d => Object.create(d));
+    createNode();
+});
+
+const scale = d3.scaleOrdinal(d3.schemeCategory10);
+const color = d => scale(d.group);
+const ticked = () => {
+    console.log()
+    link
+        .attr("x1", d => d.source.x)
+        .attr("y1", d => d.source.y)
+        .attr("x2", d => d.target.x)
+        .attr("y2", d => d.target.y);
+
+    node
+        .attr("cx", d => d.x)
+        .attr("cy", d => d.y);
+}
+const simulation = d3
+    .forceSimulation(nodes)
+    .force("link", d3.forceLink(links).id(d => d.id))
+    .force("charge", d3.forceManyBody())
+    .force("center", d3.forceCenter(w / 2, h / 2))
+    // .on("tick", ticked);
+let drag = simulation => {
+
+    function dragstarted(d) {
+        if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+        d.fx = d.x;
+        d.fy = d.y;
+    }
+
+    function dragged(d) {
+        d.fx = d3.event.x;
+        d.fy = d3.event.y;
+    }
+
+    function dragended(d) {
+        if (!d3.event.active) simulation.alphaTarget(0);
+        d.fx = null;
+        d.fy = null;
+    }
+
+    return d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended);
+}
+const relationSvg = d3
+    .select('#force')
+    .append('svg')
+    .attr('width', w)
+    .attr('height', h);
+
+let createLine = () => {
+
+    link = relationSvg.append("g")
+        .attr("stroke", "#999")
+        .attr("stroke-opacity", 0.6)
+        .selectAll("line")
+        .data(links)
+        .enter().append("line")
+        .attr("stroke-width", d => Math.sqrt(d.value));
+    link
+        .attr("x1", d => d.source.x)
+        .attr("y1", d => d.source.y)
+        .attr("x2", d => d.target.x)
+        .attr("y2", d => d.target.y);
+        console.log(link)
+}
+
+let createNode = () => {
+
+    node = relationSvg.append("g")
+        .attr("stroke", "#fff")
+        .attr("stroke-width", 1.5)
+        .selectAll("circle")
+        .data(nodes)
+        .enter().append("circle")
+        .attr("r", 5)
+        .attr("fill", color)
+        .call(drag(simulation));
+
+    node.append("title")
+        .text(d => d.id);
+        node
+        .attr("cx", d => d.x)
+        .attr("cy", d => d.y);
+}
+
+
+
+
+//#endregion
